3d2c5e02313e41c06e332847dfd67d6b64626329
Radman Hayati <radman.hayatii@gmail.com>
2021-03-13 19:12:47 +0330

    launch/job/job's state

--- app/src/main/java/com/anushka/coroutinesRadmanHayati/MainActivity.kt
+++ app/src/main/java/com/anushka/coroutinesRadmanHayati/MainActivity.kt
@@ -8,20 +8,46 @@ import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
-    private var count = 0
+    lateinit var job1: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CoroutineScope(Dispatchers.IO).launch {
-            Log.i("MyTag", "calculation jus")
-            var stock1 = async { getStuck1() }
-            var stock2 = async { getStuck2() }
-           val total = stock1.await() + stock2.await()
-            Log.i("MyTag", "total is $total")
+        /*   CoroutineScope(Dispatchers.IO).launch {
+               Log.i("MyTag", "calculation just started")
+               var stock1 = async { getStuck1() }
+               var stock2 = async { getStuck2() }
+              val total = stock1.await() + stock2.await()
+               Log.i("MyTag", "total is $total")
+           }*/
+        job1 = CoroutineScope(Dispatchers.Main).launch {
+            downloadData()
+        }
+        cancelButton.setOnClickListener(){
+            job1.cancel()
+        }
+        statusButton.setOnClickListener(){
+            if (job1.isActive){
+                textView.text="active"
+            }else if (job1.isCancelled){
+                textView.text="canceled"
+            }else if (job1.isCompleted){
+                textView.text="completed"
+            }
         }
     }
+
+    private suspend fun downloadData() {
+        withContext(Dispatchers.IO) {
+            repeat(30) {
+                delay(1000)
+                Log.i("MYTAG", "repeating $it")
+            }
+        }
+    }
+
 }
+/*
 private suspend fun getStuck1(): Int {
     delay(10000)
     Log.i("MyTag", "stock1 returned")
@@ -32,6 +58,7 @@ private suspend fun getStuck2(): Int {
     Log.i("MyTag", "stock2 returned")
     return 1111
 }
+*/
 
 
 /*
